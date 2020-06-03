6de16776be79b6b903287aee20e52ae75c42a814
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-06-03 12:02:10 -0300

    Routing between pages

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { FiLogIn } from 'react-icons/fi';
 import "./styles.css";
 
 import logo from "../../assets/logo.svg";
+import { Link } from "react-router-dom";
 
 const Home = () => {
 	return (
@@ -16,10 +18,10 @@ const Home = () => {
 						Ajudamos pessoas a encontrarem pontos de coleta de forma
 						eficiente
 					</p>
-					<a href="/cadastro">
-						<span>></span>
+					<Link to="/cadastro">
+						<span><FiLogIn /></span>
 						<strong>Cadastre um ponto de coleta</strong>
-					</a>
+					</Link>
 				</main>
 			</div>
 		</div>
