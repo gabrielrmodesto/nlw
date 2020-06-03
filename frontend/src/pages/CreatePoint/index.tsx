60c1ed3fa1f5d3da148999eadfcae6ba4e6fd9ae
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-06-03 15:01:09 -0300

    Connect with my api

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 import logo from "../../assets/logo.svg";
 import { FiArrowLeft } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import { Map, TileLayer, Marker } from "react-leaflet";
+import api from "../../services/api";
+
+interface Item {
+	id: number;
+	title: string;
+	image_url: string;
+}
 
 const CreatePoint = () => {
+	const [items, setItems] = useState<Item[]>([]);
+	useEffect(() => {
+		api.get("items").then((response) => {
+			setItems(response.data);
+		});
+	}, []);
 	return (
 		<div id="page-create-point">
 			<header>
@@ -72,30 +85,12 @@ const CreatePoint = () => {
 						<span>Selecione um ou mais itens abaixo</span>
 					</legend>
 					<ul className="items-grid">
-						<li>
-							<img src="" alt="" />
-							<span></span>
-						</li>
-						<li>
-							<img src="" alt="" />
-							<span></span>
-						</li>
-						<li>
-							<img src="" alt="" />
-							<span></span>
-						</li>
-						<li>
-							<img src="" alt="" />
-							<span></span>
-						</li>
-						<li>
-							<img src="" alt="" />
-							<span></span>
-						</li>
-						<li>
-							<img src="" alt="" />
-							<span></span>
-						</li>
+						{items.map((item) => (
+							<li key={item.id}>
+								<img src={item.image_url} alt={item.title} />
+								<span>{item.title}</span>
+							</li>
+						))}
 					</ul>
 				</fieldset>
 				<button type="submit">Cadastrar ponto de coleta</button>
