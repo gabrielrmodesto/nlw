fc388f8e091255c8389786ff6dad1a948060838b
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-06-03 12:38:25 -0300

    Register form

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -1,7 +1,99 @@
-import React from 'react';
+import React from "react";
+import "./style.css";
+import logo from "../../assets/logo.svg";
+import { FiArrowLeft } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 const CreatePoint = () => {
-  return <h1>Hello</h1>;
-}
+	return (
+		<div id="page-create-point">
+			<header>
+				<img src={logo} alt="Ecoleta" />
+				<Link to="/">
+					<FiArrowLeft />
+					<span>Voltar para home</span>
+				</Link>
+			</header>
+			<form>
+				<h1>
+					Cadastro do
+					<br /> ponto de coleta
+				</h1>
+				<fieldset>
+					<legend>
+						<h2>Dados</h2>
+					</legend>
+					<div className="field">
+						<label htmlFor="name">Nome da entidade</label>
+						<input type="text" name="name" id="name" />
+					</div>
+					<div className="field-group">
+						<div className="field">
+							<label htmlFor="email">E-mail</label>
+							<input type="email" name="email" id="email" />
+						</div>
+						<div className="field">
+							<label htmlFor="whatsapp">Whatsapp</label>
+							<input type="text" name="whatsapp" id="whatsapp" />
+						</div>
+					</div>
+				</fieldset>
+				<fieldset>
+					<legend>
+						<h2>Endereço</h2>
+						<span>Selecione o endereço no mapa</span>
+					</legend>
+					<div className="field-group">
+						<div className="field">
+							<label htmlFor="uf">Estado (UF)</label>
+							<select name="uf" id="uf">
+								<option value="0">Selecione o estado</option>
+							</select>
+						</div>
+						<div className="field">
+							<label htmlFor="city">Cidade</label>
+							<select name="city" id="city">
+								<option value="0">Selecione a cidade</option>
+							</select>
+						</div>
+					</div>
+				</fieldset>
+				<fieldset>
+					<legend>
+						<h2>Itens de coleta</h2>
+						<span>Selecione um ou mais itens abaixo</span>
+					</legend>
+					<ul className="items-grid">
+						<li>
+							<img src="" alt="" />
+							<span></span>
+						</li>
+						<li>
+							<img src="" alt="" />
+							<span></span>
+						</li>
+						<li>
+							<img src="" alt="" />
+							<span></span>
+						</li>
+						<li>
+							<img src="" alt="" />
+							<span></span>
+						</li>
+						<li>
+							<img src="" alt="" />
+							<span></span>
+						</li>
+						<li>
+							<img src="" alt="" />
+							<span></span>
+						</li>
+					</ul>
+				</fieldset>
+				<button type="submit">Cadastrar ponto de coleta</button>
+			</form>
+		</div>
+	);
+};
 
 export default CreatePoint;
