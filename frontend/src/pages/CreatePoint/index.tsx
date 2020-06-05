1ed2527e7d410ae54873bad88bc5933563f99ec8
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-06-05 16:52:08 -0300

    Validate the fields

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -118,17 +118,15 @@ const CreatePoint = () => {
 
 		const data = new FormData();
 
-		
-			data.append('name',name);
-			data.append('email',email);
-			data.append('whatsapp',whatsapp);
-			data.append('uf',uf);
-			data.append('city',city);
-			data.append('latitude',String(latitude));
-			data.append('longitude',String(longitude));
-			data.append('items',items.join(','));
-			if(selectedFile) data.append('image', selectedFile);
-		
+		data.append("name", name);
+		data.append("email", email);
+		data.append("whatsapp", whatsapp);
+		data.append("uf", uf);
+		data.append("city", city);
+		data.append("latitude", String(latitude));
+		data.append("longitude", String(longitude));
+		data.append("items", items.join(","));
+		if (selectedFile) data.append("image", selectedFile);
 
 		await api.post("points", data);
 
